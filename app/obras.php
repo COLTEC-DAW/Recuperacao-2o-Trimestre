37dc3f5ace9a5f65c35c029614a6c2d6467e976b
IcaroColtec <icaroemd18@outlook.com>
2018-09-27 10:36:17 -0300

    ajustes nas mensagens personalizadas

--- app/obras.php
+++ app/obras.php
@@ -17,17 +17,20 @@ class obras extends Model
 
     public $messagesObras = [
 
-        'nome.required'     => 'O livro precisa ter um nome',
-        'nome.min'          => 'O nome precisa conter no mínimo 3 caracteres',
-        'nome.max'          => 'O nome pode conter no máximo 45 caracteres',
-        'nome.unique'       => 'Esse livro já foi cadastrado',
-        'resumo.required'   => 'O livro deve conter um resumo',
-        'resumo.unique'     => 'Esse resumo já existe',
-        'resumo.min'        => 'O resumo deve conter no mínimo 10 caracteres',
-        'resumo.max'        => 'O resumo pode conter no máximo 100 caracteres',
-        'editora.required'  => 'Uma editora deve estar relacionada ao livro',
-        'editora.min'       => 'O nome da editora deve conter no mínimo 1 caracteres',
-        'editora.max'       => 'O nome da editora pode conter no máximo 45 caracteres',
+        'nome.required'              => 'O livro precisa ter um nome',
+        'nome.min'                   => 'O nome precisa conter no mínimo 3 caracteres',
+        'nome.max'                   => 'O nome pode conter no máximo 45 caracteres',
+        'nome.unique'                => 'Esse livro já foi cadastrado',
+        'resumo.required'            => 'O livro deve conter um resumo',
+        'resumo.unique'              => 'Esse resumo já existe',
+        'resumo.min'                 => 'O resumo deve conter no mínimo 10 caracteres',
+        'resumo.max'                 => 'O resumo pode conter no máximo 100 caracteres',
+        'editora.required'           => 'Uma editora deve estar relacionada ao livro',
+        'editora.min'                => 'O nome da editora deve conter no mínimo 1 caracteres',
+        'editora.max'                => 'O nome da editora pode conter no máximo 45 caracteres',
+        'num_exemplares.required'    => 'Número de exemplares requerido',
+        'num_exemplares.integer'     => 'O número de exemplares deve ser inteiro',
+        
 
     ];
 
