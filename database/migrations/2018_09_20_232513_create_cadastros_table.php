db60c13635732f5b994d0f180541765ba64606f7
icaro <icaroemd18@outlook.com>
2018-09-28 18:41:26 -0300

    alterando o tamanho da senha na db

--- database/migrations/2018_09_20_232513_create_cadastros_table.php
+++ database/migrations/2018_09_20_232513_create_cadastros_table.php
@@ -17,7 +17,7 @@ class CreateCadastrosTable extends Migration
             $table->increments('id');
             $table->string('Name','45');
             $table->string('Email');
-            $table->string('Password','8');
+            $table->string('Password');
             $table->timestamps();
         });
     }
