23dbed146eac1cb2e90a4c9a7c4b492f57c5be38
IcaroColtec <icaroemd18@outlook.com>
2018-10-01 11:01:30 -0300

    nova estilização

--- resources/views/includes/menu.blade.php
+++ resources/views/includes/menu.blade.php
@@ -1,15 +1,19 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="{{URL::to('/Home')}}"><h5><b>MyReads COLTEC</b></h5></a>
-        <ul class="navbar-nav mr-auto "></ul>   
-              
-        <div id="custom-search-input">
-            <div class="input-group col-md-12 ">
-                <form action="{{URL::to('/pesquisar')}}" method="post">
-                    <input name="busca" type="text" class="form-control input-lg" placeholder="Nome,autor e editora..." />
-                </form>
-            </div>
+
+ <nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidebar" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="{{URL::to('/Home')}}"><h5><b>MyReads COLTEC</b></h5></a>
+
+    <div id="custom-search-input">
+        <div class="input-group">
+            <form action="{{URL::to('/pesquisar')}}" method="post">
+                <input name="busca" type="text" class="form-control input-lg" placeholder="Nome,autor e editora..." />
+            </form>
         </div>
-        
-        <h5 id="titleuser">Bom dia!</h5>
-        <a href="/sair" class="btn btn-outline-success" type="button">Log Out</a>
+    </div>
+
+    <h5 id="titleuser">Bom dia!</h5>
+    <a href="/sair" id="logoutButton" class="btn btn-outline-success" type="button">Log Out</a>
 </nav>
+
