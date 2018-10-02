c5dc154530ba401a80b380db1ca2484aef03f4cf
IcaroColtec <icaroemd18@outlook.com>
2018-10-02 07:41:38 -0300

    deixando o código html limpo

--- resources/views/templates/template2.blade.php
+++ resources/views/templates/template2.blade.php
@@ -3,44 +3,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?php echo asset('css/style2.css')?>" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Página</title>
-
-     <style>
-    
-        body{
-        background: #3B7F78;
-        }
-
-        #bienvenido{
-            margin-top: 5%;
-        }
-
-        #container_login{
-            min-height: 100%;
-            display: flex;
-            align-items: center;
-        }
-
-        #box{
-            background-color: #EBEBEB;
-            border: solid #E8E8E8;
-            border-radius: 5px;
-        }
-
-        #formulario{
-            margin-top: 15%;
-            margin-bottom: 5%;
-            padding-left: 8%;
-            padding-right: 8%; 
-        }
-
-        #buttons_box{
-            margin-bottom: 10%;
-        }
-            
-    </style>
-
 </head>
 <body>
     
