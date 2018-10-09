8ee077451569b125ba77488dc64da98ac47c2d75
GustavinhoHenrique <ujhenrique@gmail.com>
2018-10-09 11:36:51 -0300

    tentativa da esperança suprema kkk

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,37 @@
-@extends('layouts.app')
+@extends('templates.template1')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Página home</div>
+    <div class="container-fluid">
+        
+        @if (session('inserido'))
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <table>
-                        <tr>
-                        </tr>
-                        @foreach($obras as $obrasp)
-                        <tr>
-                            <td>{{$obrasp->nome}}</td>
-                            <td>{{$obrasp->resumo}}</td>
-                            <td>{{$obrasp->autor}}</td>
-                            <td>{{$obrasp->editora}}</td>
-                            <td>{{$obrasp->exemplares}}</td>
-                        </tr>
-                        @endforeach
-                    <a href="/cadastrar"><br>Adicionar Obras</a>
-                </div>
-            </div>
+        <br/>
+        <div class="alert alert-success col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+            {{ session('inserido') }}
         </div>
+
+        @endif
+        <br/>
+        
+        <h1 id="title">Últimas Obras</h1>
+        <br/>
+
+        @foreach($obras as $obra)
+            <div class="list-group col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                <a href="#" class="list-group-item list-group-item-action flex-column align-items-start card">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">{{$obra->nome}}</h4>
+                    </div>
+                    <p class="mb-1">{{$obra->resumo}}</p> 
+                    <br/>
+                    <small>{{$obra->autor}}</small>
+                    <div class="d-flex w-100 justify-content-between">
+                        <small>Editora: {{$obra->editora}}</small>
+                        <small>N° Exemplares: {{$obra->exemplares}}</small>
+                    </div>
+                </a>
+            </div>
+            <br/>
+        @endforeach
     </div>
-</div>
 @endsection
