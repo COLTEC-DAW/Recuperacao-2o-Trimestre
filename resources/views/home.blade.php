5e014eb68bfc8357e00c7a6d503d2d5dc78690e5
GustavinhoHenrique <ujhenrique@gmail.com>
2018-10-10 06:33:39 -0300

    adaptação ao template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,9 @@
 @section('padrao')
 
             @foreach($obras as $obrasp)
+            {{ csrf_field() }}
                 <div class="list-group col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start card">
+                    <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
                         <div class="d-flex w-100 justify-content-between">
                             <h4 class="mb-1">{{$obrasp->nome}}</h4>
                         </div>
@@ -12,13 +13,41 @@
                         <br/>
                         <small>{{$obrasp->autor}}</small>
                         <div class="d-flex w-100 justify-content-between">
-                            <small>Editora: {{$obrasp->editora}}</small>
-                            <small>N° Exemplares: {{$obrasp->exemplares}}</small>
+                            <h6>Editora:</h6>
+                            <small> {{$obrasp->editora}}</small>
+                            <small>Número de exemplares disponíveis: {{$obrasp->exemplares}}</small>
                         </div>
                     </a>
                 </div>
                 <br/>
             @endforeach
+
+            <div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small>3 days ago</small>
+    </div>
+    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small>Donec id elit non mi porta.</small>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small class="text-muted">Donec id elit non mi porta.</small>
+  </a>
+  <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+    <div class="d-flex w-100 justify-content-between">
+      <h5 class="mb-1">List group item heading</h5>
+      <small class="text-muted">3 days ago</small>
+    </div>
+    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+    <small class="text-muted">Donec id elit non mi porta.</small>
+  </a>
+</div>
 
 
 
