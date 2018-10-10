ef1fd2a493a06db68fa12502a1194b69a85b6a11
GustavinhoHenrique <ujhenrique@gmail.com>
2018-10-10 07:22:36 -0300

    Commit voltando à época que as funções estavam funcionando

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,38 @@
 
 @section('content')
 
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>BiblioRec</title>
+     <style>
+        #barrinha {
+            background-color: #b5d9f3;
+        }
 
-    <body>
+        #titulo{
+            font-weight: bolder;
+        }
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-light" id="barrinha">
+        <a class="navbar-brand" href="#" id="titulo">
+            <img src="coltec.png" width="85" height="50" class="d-inline-block align-top" alt="">
+                Biblioteca do Coltec
+                </a>
+                <span class="navbar-text">
+                    Seja bem vindo ao nosso sistema bibliotecário!
+                  </span>
+                <form class="form-inline" action = "{{url('/pesquisa')}}" method = "post">
+                    {{ csrf_field() }}
+                    <input  class="form-control mr-sm-2" name="busca" type="search" placeholder="Digite sua busca" aria-label="Search">
+                    <button class="btn s my-2 my-sm-0 btn-primary" id="botao" type="submit">Pesquisar</button>
+                  </form>
+            </nav>
 
             @foreach($obras as $obrasp)
                 <div class="list-group col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -23,9 +53,19 @@
                 <br/>
             @endforeach
 
-
-    </body>
+<!--------------------------------------------------------------------------------------------->    
+    <div class="row">
+        <div class="col-12 col-sm-5 col-md-4 col-xl-2" id="agr">
+            <div id="list" class="list-group">
+                <a class="list-group-item list-group-item-action nav-link list-group-item-primary" href="/cadastrar">Cadastrar Obras</a>
+                <a class="list-group-item list-group-item-action nav-link list-group-item-primary" href="/">Informações</a>
+             </div>
+        </div>
+    </div>
+</body>
+</html>
 <!-- COMMIT DA VITÓRIA -->
 
 
 @endsection
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,32 +4,43 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Biblioteca da Esquina') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-
-<nav class="navbar navbar-inverse">
-            <div class="container-fluid">
+<body>
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="/">Biblioteca da Esquina | Gustavinho e Mari</a>
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
                 </div>
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="/cadastro">Cadastro de Obras</a></li>
-                    <li><a href="/">Informações</a></li>
-                </ul>
-                <form class="form-inline" action = "{{url('/pesquisa')}}" method = "post">
-                    {{ csrf_field() }}
-                    <input  class="form-control mr-sm-2" name="busca" type="search" placeholder="Digite sua busca" aria-label="Search">
-                    <button class="btn s my-2 my-sm-0 btn-primary" id="botao" type="submit">Pesquisar</button>
-                </form>
-                <ul class="nav navbar-nav navbar-right">
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
@@ -56,15 +67,14 @@
                             </li>
                         @endguest
                     </ul>
+                </div>
             </div>
         </nav>
 
-<body>
-    
+        @yield('content')
+    </div>
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
