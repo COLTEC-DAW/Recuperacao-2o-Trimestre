17135190a27f0b7f59cd1581fad20ff147d07e72
GustavinhoHenrique <ujhenrique@gmail.com>
2018-10-10 13:30:08 -0300

    sim

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,8 @@
 
         <div class="row">
             @foreach($obras as $obrasp)
-                  <div class="card col-12 col-md-6 col-lg-4" style="width: 100rem;">
+                <div class="col-12 col-md-6 col-lg-4"></div>
+                  <div class="card">
                       <div class="card-body">
                           <h5 class="card-title">{{$obrasp->nome}}</h5>
                           <h6 class="card-subtitle mb-2 text-muted">{{$obrasp->autor}}</h6>
@@ -13,6 +14,7 @@
                           <small>N° exemplares: {{$obrasp->exemplares}}</small>
                       </div>
                   </div>
+                </div>
             @endforeach
         </div>
 @endsection
