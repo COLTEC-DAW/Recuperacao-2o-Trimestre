e21c2151f8673e15ba3675ea37401769134026cf
MoleSama <73478225+MoleSama@users.noreply.github.com>
2021-12-08 12:18:37 -0300

    Classe livro criada

---
+++ src/model/book.php
@@ -0,0 +1,49 @@
+<?php
+    class book{
+
+        public $NomeObra;
+        public $ResumoObra;
+        public $NomeAutor;
+        public $NomeEditora;
+        public $NumExemplares;
+        public $DataCadastro;
+
+        public function __construct($nomeObra, $nomeAutor, $nomeEditora, $resumoObra, $numExemplares){
+
+            $this->NomeObra = $nomeObra;
+            $this->NomeAutor = $nomeAutor;
+            $this->NomeEditora = $nomeEditora;
+            $this->ResumoObra = $resumoObra;
+            $this->NumExemplares = $numExemplares;
+            $this->DataCadastro = date('d/m/Y H:i');
+        }
+
+        function GetLivros(){
+        
+            $FileJson = file_get_contents('./src/repository/Livros.json', true);
+            $decode = json_decode($FileJson, true);
+    
+            return $decode;
+        }
+
+        function PostLivro(){
+            $FileJson = file_get_contents('./src/repository/Livros.json', true);
+            $decode = json_decode($FileJson, true);
+
+            $newLivro = array(
+                "NomeObra" => $this->NomeObra,
+                "NomeAutor" => $this->NomeAutor,
+                "NomeEditora" => $this->NomeEditora,
+                "ResumoObra" => $this->ResumoObra,
+                "NumExemplares" => $this->NumExemplares,
+                "DataCadastro" => $this->DataCadastro,
+            );
+
+            $decode[] = $newLivro;
+            
+            $FileJson = json_encode($decode, JSON_PRETTY_PRINT);
+            file_put_contents("./src/repository/Livros.json", $FileJson);
+        }
+    }
+    
+?>
