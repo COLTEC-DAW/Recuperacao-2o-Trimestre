20e76fcd9719cb6a5a86498ae8e47c5c85a56a32
MoleSama <73478225+MoleSama@users.noreply.github.com>
2021-12-08 14:19:12 -0300

    Função de busca de dados criada

--- src/library/readBook.php
+++ src/library/readBook.php
@@ -1,7 +1,5 @@
 <?php
 
-    include './src/model/book.php';
-
     function ReadAllBooks(){
 
         $FileJson = file_get_contents('./src/repository/Livros.json', true);
---
+++ src/library/searchBooks.php
@@ -0,0 +1,26 @@
+<?php
+
+    include './src/model/book.php';
+
+    function SearchBooks(){
+
+        if(isset($_GET["procurado"])){
+            $FileJson = file_get_contents('./src/repository/Livros.json', true);
+            $decode = json_decode($FileJson, true);
+            $retorno = [];
+
+            foreach($decode as $livro){
+                if($livro["NomeObra"] == $_GET["procurado"] || $livro["NomeAutor"] == $_GET["procurado"] || $livro["NomeEditora"] == $_GET["procurado"]){
+                    array_push($retorno, $livro);
+                }
+            }
+
+            setcookie('booksResult', $retorno);
+
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+?>
